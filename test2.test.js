2f7e445b6476d530aa014e07c66b185f553f12e7
M. Huda <muda1707@gmail.com>
2022-09-21 01:01:00 +0700

    challenge dom

--- test2.test.js
+++ test2.test.js
@@ -1,39 +1,39 @@
-const { fireEvent } = require("@testing-library/dom");
+const { fireEvent } = require("@testing-library/dom")
 
-const { JSDOM } = require("jsdom");
-const fs = require("fs");
+const { JSDOM } = require("jsdom")
+const fs = require("fs")
 
 // Read index.html using jsdom
-const index = fs.readFileSync("index.html", "utf-8");
+const index = fs.readFileSync("index.html", "utf-8")
 // use JSDOM to parse index
 const dom = new JSDOM(index, {
   runScripts: "dangerously",
-});
-const { window } = dom;
-const { document } = window;
+})
+const { window } = dom
+const { document } = window
 
 describe("Checking the JS Function", () => {
   it("should have a function called fnClickHandler", async () => {
-    const fnClickHandler = window.fnClickHandler;
+    const fnClickHandler = window.fnClickHandler
 
-    expect(typeof fnClickHandler).toBe("function");
-  });
+    expect(typeof fnClickHandler).toBe("function")
+  })
 
   it("renders new li when button is clicked", async () => {
-    const btnAddData = document.getElementById("todo-submit");
+    const btnAddData = document.getElementById("todo-submit")
 
-    fireEvent.click(btnAddData);
+    fireEvent.click(btnAddData)
 
-    let todo = document.querySelectorAll("#todo-output li");
-    expect(todo.length).toBe(1);
-  });
+    let todo = document.querySelectorAll("#todo-output li")
+    expect(todo.length).toBe(1)
+  })
 
   it("renders new li when press enter on input", async () => {
-    const input = document.getElementById("todo-input");
+    const input = document.getElementById("todo-input")
 
-    fireEvent.keyPress(input, { key: "Enter", code: 13, charCode: 13 });
+    fireEvent.keyPress(input, { key: "Enter", code: 13, charCode: 13 })
 
-    let todo = document.querySelectorAll("#todo-output li");
-    expect(todo.length).toBe(2);
-  });
-});
+    let todo = document.querySelectorAll("#todo-output li")
+    expect(todo.length).toBe(2)
+  })
+})
